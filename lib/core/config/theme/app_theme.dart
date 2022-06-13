5b1a418e7d5db9d5adb15667b3e8a53c8b8f97b2
Hassan <hassan.shakeel@venturedive.com>
2022-06-13 21:46:47 +0500

    theme integrated

--- lib/core/config/theme/app_theme.dart
+++ lib/core/config/theme/app_theme.dart
@@ -1,5 +1,7 @@
 part of 'theme.dart';
 
 abstract class AppTheme {
-  ThemeData get themeData => ThemeData();
+  ColorScheme get colorScheme;
+
+  ThemeData get themeData => ThemeData(colorScheme: colorScheme);
 }
--- lib/core/config/theme/red_theme.dart
+++ lib/core/config/theme/red_theme.dart
@@ -1,3 +1,6 @@
 part of 'theme.dart';
 
-class RedTheme extends AppTheme {}
+class RedTheme extends AppTheme {
+  @override
+  ColorScheme get colorScheme => const ColorScheme.light(primary: Colors.red);
+}
