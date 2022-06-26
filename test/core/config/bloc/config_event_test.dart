df1e9a81a6a16220ed3fccdfe3b8f43d31850901
Hassan <hassan.shakeel@venturedive.com>
2022-06-26 21:18:42 +0500

    themeEvent test

--- test/core/config/bloc/config_event_test.dart
+++ test/core/config/bloc/config_event_test.dart
@@ -1,1 +1,19 @@
+import 'package:buy_movies/core/config/bloc/config_bloc.dart';
+import 'package:buy_movies/core/config/theme/theme.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+void main() {
+  group('ConfigEvent', () {
+    const theme = ThemeEvent(themeName: ThemeName.blue);
+
+    group('ThemeEvent', () {
+      test('Initialisation', () {
+        expect(
+          theme,
+          isA<ThemeEvent>()
+              .having((p0) => p0.themeName, 'themeName', ThemeName.blue),
+        );
+      });
+    });
+  });
+}
