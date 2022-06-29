c49a325027245842cdf564182157c14a3fdbcb92
Hassan <hassan.shakeel@venturedive.com>
2022-06-29 17:03:04 +0500

    fetch movie error state

--- lib/core/exception/custom_exception.dart
+++ lib/core/exception/custom_exception.dart
@@ -1,8 +1,13 @@
-class CustomException implements Exception {
+import 'package:equatable/equatable.dart';
+
+class CustomException extends Equatable implements Exception {
   const CustomException(this.message);
 
   final String message;
 
   @override
   String toString() => message;
+
+  @override
+  List<Object?> get props => [message];
 }
--- test/modules/movie_list/bloc/movie_bloc_test.dart
+++ test/modules/movie_list/bloc/movie_bloc_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
+import 'package:buy_movies/core/exception/custom_exception.dart';
 import 'package:buy_movies/core/network/api_state.dart';
 import 'package:buy_movies/modules/movie_list/bloc/movie_bloc.dart';
 import 'package:buy_movies/modules/movie_list/repository/repository.dart';
@@ -16,7 +17,7 @@ void main() {
 
   group('MovieBloc', () {
     blocTest<MovieBloc, MovieState>(
-      'emits [MovieState.movieApiState] when FetchMovieEvent is added.',
+      'emits [MovieState.movieApiState.success] when FetchMovieEvent is added.',
       setUp: () => when(() => movieRepository.fetchMovies(any()))
           .thenAnswer((invocation) async => []),
       build: () => MovieBloc(movieRepository),
@@ -31,6 +32,25 @@ void main() {
             response: [],
           ),
           cartItems: [],
+        ),
+      ],
+    );
+
+    blocTest<MovieBloc, MovieState>(
+      'emits [MovieState.movieApiState.failure] when FetchMovieEvent is added.',
+      setUp: () => when(() => movieRepository.fetchMovies(any()))
+          .thenThrow(const CustomException('message')),
+      build: () => MovieBloc(movieRepository),
+      act: (bloc) => bloc.add(const FetchMovieEvent('test')),
+      expect: () => const <MovieState>[
+        MovieState(
+          movieApiState: ApiState(apiResponseState: ApiResponseState.loading),
+        ),
+        MovieState(
+          movieApiState: ApiState(
+            apiResponseState: ApiResponseState.error,
+            e: CustomException('message'),
+          ),
         ),
       ],
     );
