6e365cb59f845121fa69c44599b8a9df694b1643
Hassan <hassan.shakeel@venturedive.com>
2022-06-30 10:20:38 +0500

    add remove movies from cart

--- test/modules/movie_list/bloc/movie_bloc_test.dart
+++ test/modules/movie_list/bloc/movie_bloc_test.dart
@@ -16,43 +16,129 @@ void main() {
   });
 
   group('MovieBloc', () {
-    blocTest<MovieBloc, MovieState>(
-      'emits [MovieState.movieApiState.success] when FetchMovieEvent is added.',
-      setUp: () => when(() => movieRepository.fetchMovies(any()))
-          .thenAnswer((invocation) async => []),
-      build: () => MovieBloc(movieRepository),
-      act: (bloc) => bloc.add(const FetchMovieEvent('test')),
-      expect: () => const <MovieState>[
-        MovieState(
-          movieApiState: ApiState(apiResponseState: ApiResponseState.loading),
-        ),
-        MovieState(
-          movieApiState: ApiState(
-            apiResponseState: ApiResponseState.success,
-            response: [],
+    group('Fetch Movie', () {
+      blocTest<MovieBloc, MovieState>(
+        'emits [MovieState.movieApiState.success] when FetchMovieEvent is added.',
+        setUp: () => when(() => movieRepository.fetchMovies(any()))
+            .thenAnswer((invocation) async => []),
+        build: () => MovieBloc(movieRepository),
+        act: (bloc) => bloc.add(const FetchMovieEvent('test')),
+        expect: () => const <MovieState>[
+          MovieState(
+            movieApiState: ApiState(apiResponseState: ApiResponseState.loading),
           ),
-          cartItems: [],
-        ),
-      ],
-    );
+          MovieState(
+            movieApiState: ApiState(
+              apiResponseState: ApiResponseState.success,
+              response: [],
+            ),
+            cartItems: [],
+          ),
+        ],
+      );
 
-    blocTest<MovieBloc, MovieState>(
-      'emits [MovieState.movieApiState.failure] when FetchMovieEvent is added.',
-      setUp: () => when(() => movieRepository.fetchMovies(any()))
-          .thenThrow(const CustomException('message')),
-      build: () => MovieBloc(movieRepository),
-      act: (bloc) => bloc.add(const FetchMovieEvent('test')),
-      expect: () => const <MovieState>[
-        MovieState(
-          movieApiState: ApiState(apiResponseState: ApiResponseState.loading),
-        ),
-        MovieState(
-          movieApiState: ApiState(
-            apiResponseState: ApiResponseState.error,
-            e: CustomException('message'),
+      blocTest<MovieBloc, MovieState>(
+        'emits [MovieState.movieApiState.failure] when FetchMovieEvent is added.',
+        setUp: () => when(() => movieRepository.fetchMovies(any()))
+            .thenThrow(const CustomException('message')),
+        build: () => MovieBloc(movieRepository),
+        act: (bloc) => bloc.add(const FetchMovieEvent('test')),
+        expect: () => const <MovieState>[
+          MovieState(
+            movieApiState: ApiState(apiResponseState: ApiResponseState.loading),
           ),
+          MovieState(
+            movieApiState: ApiState(
+              apiResponseState: ApiResponseState.error,
+              e: CustomException('message'),
+            ),
+          ),
+        ],
+      );
+    });
+
+    group('Add/Remove Movie to Cart', () {
+      const drStrangers = MovieModel(
+        price: 12,
+        movieName: 'Dr Strangers',
+        imageUrl: 'www.disneyplus.com',
+      );
+
+      const msMarvel = MovieModel(
+        price: 120,
+        movieName: 'Ms. Marvel',
+        imageUrl: 'www.disneyplus.com',
+      );
+
+      const lostInSpace = MovieModel(
+        price: 130,
+        movieName: 'Lost in Space',
+        imageUrl: 'www.netflix.com',
+      );
+
+      const startup = MovieModel(
+        price: 240,
+        movieName: 'Startup',
+        imageUrl: 'www.netflix.com',
+      );
+
+      const the100 = MovieModel(
+        price: 340,
+        movieName: 'The 100',
+        imageUrl: 'www.netflix.com',
+      );
+
+      const seedState = MovieState(
+        movieApiState: ApiState(
+          apiResponseState: ApiResponseState.success,
+          response: [
+            drStrangers,
+            msMarvel,
+            lostInSpace,
+            startup,
+            the100,
+          ],
         ),
-      ],
-    );
+      );
+
+      blocTest<MovieBloc, MovieState>(
+        'emits [MovieState.movieApiState.success] when AddMovieToCartEvent is added.',
+        seed: () => seedState,
+        build: () => MovieBloc(movieRepository),
+        act: (bloc) => bloc
+          ..add(
+            const AddMovieToCartEvent(drStrangers, 'apiKey'),
+          )
+          ..add(
+            const AddMovieToCartEvent(drStrangers, 'apiKey'),
+          )
+          ..add(
+            const AddMovieToCartEvent(msMarvel, 'apiKey'),
+          ),
+        expect: () => <MovieState>[
+          seedState.copyWith(cartItems: [drStrangers]),
+          seedState.copyWith(cartItems: [drStrangers, drStrangers]),
+          seedState.copyWith(cartItems: [msMarvel, drStrangers, drStrangers]),
+        ],
+      );
+
+      blocTest<MovieBloc, MovieState>(
+        'emits [MovieState.movieApiState.success] when AddMovieToCartEvent is added.',
+        seed: () =>
+            seedState.copyWith(cartItems: [msMarvel, drStrangers, drStrangers]),
+        build: () => MovieBloc(movieRepository),
+        act: (bloc) => bloc
+          ..add(
+            const RemoveMovieFromCartEvent(drStrangers, 'apiKey'),
+          )
+          ..add(
+            const RemoveMovieFromCartEvent(msMarvel, 'apiKey'),
+          ),
+        expect: () => <MovieState>[
+          seedState.copyWith(cartItems: [msMarvel, drStrangers]),
+          seedState.copyWith(cartItems: [drStrangers]),
+        ],
+      );
+    });
   });
 }
